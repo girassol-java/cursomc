457501350bff3df78d0975d9d5463a9ad8314edc
Felipe <girassol.java@gmail.com>
2019-09-26 10:37:10 -0300

    Testando a 1a classe de dominio 'categoria'

--- src/main/java/br/com/girassol/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/girassol/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package br.com.girassol.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.girassol.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
+	public List<Categoria> listar() {
 		
-		return "REST está funcionando";
+		Categoria cat1 = new Categoria(1,"Informática");
+		Categoria cat2 = new Categoria(1,"Escritório");
+		
+		List<Categoria> listaCat = new ArrayList<>();
+		listaCat.add(cat1);
+		listaCat.add(cat2);
+		
+		return listaCat;
 	}
 }
